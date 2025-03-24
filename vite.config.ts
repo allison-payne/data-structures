971e543f0db7d47fd562d8d47366f20b4ef626a4
Allison Payne <allison@apayneinthe.app>
2025-03-24 10:11:57 -0400

    Updates Base

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: '/data-structures/',
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
