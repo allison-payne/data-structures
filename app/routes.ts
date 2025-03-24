ea5d1ca5f815713b4ab3d2ce85c91c8ea325f34e
Allison Payne <allison@apayneinthe.app>
2025-03-24 10:46:06 -0400

    Fixes routing

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,5 @@
-import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
+import { type RouteConfig, index, layout, prefix, route } from "@react-router/dev/routes";
 
-export default [layout('layout.tsx', [index("routes/home.tsx"), route('binary-search-tree', 'routes/bst.tsx')])] satisfies RouteConfig;
+export default [
+    ...prefix("data-structures", [layout('layout.tsx', [index("routes/home.tsx"), route('binary-search-tree', 'routes/bst.tsx')])])
+] satisfies RouteConfig;
