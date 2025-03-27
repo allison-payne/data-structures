c10a1b57f865ede3974c486c0498964f65c8b867
Allison Payne <allison@apayneinthe.app>
2025-03-27 19:34:10 -0400

    Adds Pan and zoom

--- app/components/binary-tree-viewer/binary-tree-viewer.tsx
+++ app/components/binary-tree-viewer/binary-tree-viewer.tsx
@@ -1,48 +1,24 @@
 import type { JSX } from "react";
-import React, { useCallback, useState } from "react";
+import React from "react";
 import type { TreeNode } from "~/structures/binary-tree/TreeNode";
 import SVG from "../svg";
 import { initialViewBox } from "../svg/constants";
 import TreeBranch from "../svg/tree-branch";
 import TreeNodeSVG from "../svg/tree-node";
-import { ZoomSlider } from "./styles";
 
 export type BinaryTreeSVGProps<T> = {
     tree: Array<TreeNode<T>>;
 }
 
 export function BinaryTreeViewer<T>({ tree }: BinaryTreeSVGProps<T>): JSX.Element {
-    //const [orderedNodes, setOrderedNodes] = useState<Array<TreeNode<T>>>();
     const getScaledCoords = (coordValue: number): string => {
         return `${coordValue * initialViewBox}`
     }
 
-    const [zoom, setZoom] = useState(initialViewBox);
-
-    const handleZoomChange = useCallback((e: any) => {
-        setZoom(e.target?.value)
-    }, [])
-
-    // useEffect(()=>{
-    //     setOrderedNodes([...tree.inOrder()]);
-    // },[tree, setOrderedNodes])
-
     return (
         <>
-            <div className="h-[600px] align-top p">
-                <label className="block" htmlFor="zoom">Zoom</label>
-                <ZoomSlider
-                    type="range"
-                    id="zoom"
-                    name="zoom"
-                    min="50"
-                    max="250"
-                    value={zoom}
-                    step="10"
-                    onChange={handleZoomChange} />
-            </div>
             <div className="w-[600px]">
-                <SVG zoom={zoom}>
+                <SVG>
                     {tree?.map((node, index) => {
                         return (<React.Fragment key={`${index}`}>
                             {node.left &&
--- app/components/svg/svg.tsx
+++ app/components/svg/svg.tsx
@@ -1,12 +1,43 @@
-import { type ReactNode } from "react";
+import { useEffect, useRef, useState, type ReactNode } from "react";
+import { ReactSVGPanZoom, TOOL_NONE, type Tool, type Value } from "react-svg-pan-zoom";
 
 export type SVGProps = {
     children: ReactNode
-    zoom: number
 }
 
-const SVG = ({ zoom, children }: SVGProps) => {
-    return <svg viewBox={`0 0 ${zoom} ${zoom}`}>{children}</svg>  
+const SVG = ({ children }: SVGProps) => {
+    const Viewer = useRef<ReactSVGPanZoom>(null);
+    const [tool, setTool] = useState<Tool>(TOOL_NONE);
+    const [value, setValue] = useState<Value | null>(null);
+
+    useEffect(() => {
+        Viewer.current && Viewer.current.fitToViewer();
+    }, [Viewer]);
+
+    const setValueHandler = (e) => {
+        console.log(e);
+        setValue(e);
+    };
+
+    return (
+        <ReactSVGPanZoom
+            ref={Viewer}
+            background="grey"
+            width={600}
+            height={600}
+            tool={tool}
+            onChangeTool={setTool}
+            preventPanOutside={true}
+            SVGBackground="grey"
+            value={value}
+            onChangeValue={setValueHandler}
+            onClick={(event) =>
+                console.log("click", event.x, event.y, event.originalEvent)
+            }
+        >
+            <svg viewBox={`0 0 ${100} ${100}`}>{children}</svg>
+        </ReactSVGPanZoom>
+    )
 }
 
 export default SVG
